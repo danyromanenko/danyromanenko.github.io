66eb16a7f282ae59f617eb1e49e73d14c3c78786
Dany Ro <romanenko661@gmail.com>
2016-04-01 17:21:07 +0400

    changes

--- js1-2/task2/js/script.js
+++ js1-2/task2/js/script.js
@@ -1,9 +1,18 @@
-(function(){
+(function nameList() {
     var i;
     var arr = [];
     for (i = 1; i <= 5; i++) {
-        arr.push(prompt('Введите имя №'+i));
+        arr.push(prompt('Введите имя №' + i));
     }
     var name = prompt('Введите ваше имя пользователя');
+    
+    function nameCompare(name) {
+        for (var i = 0; i < nameList.length; ++i) {
+        if (nameList[i] === name) {
+        return "Your name: " + nameList[i]
+    }
+  }
+    return false;
+}
     (arr.indexOf(name) != -1) ? (alert(name+', вы успешно вошли')) : (alert('Имя '+name+' не найдено'));
 })();
